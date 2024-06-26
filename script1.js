98d3a551eb5acbc4be4cc4c67274740b31095dbd
Nayan Hossain <nayan64@student.sust.edu>
2024-06-27 02:49:41 +0600

    modified

--- script1.js
+++ script1.js
@@ -1,3 +1,11 @@
 //Number
 
 //console.log("HELLO")
+student = ["nayan", 2,{name:"nayan", age:23, id:["nayan",{id:23, sr:"name",name:["Nayan","Hossain"],distic:"magura"},"hossain","attick"], home:"magura"},"Osman"] 
+console.log(student);
+
+var num = ["123"];
+var num1 = num;
+console.log(num1);
+num  [0] = 4555
+console.log(num1)
